f947ad525d944c2491636fef288ac0e4b06279e0
Florian Ritterhoff <florian@ritterhoff.org>
2022-03-23 16:12:52 +0100

    tests: fix inital test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Anmelden/i);
+
+  await waitFor(() => {
+    expect(linkElement).toBeInTheDocument();
+  });
 });
