851c82f269628028301f3ddab19ad9d94211a8c4
Florian Ritterhoff <florian@ritterhoff.org>
2022-03-30 08:01:22 +0200

    tests: use tsdx instead of react-scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend for PKI-Portal
+
+This project provides the frontend for the certificate self-service portal.
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    globals: {
+        crypto: require("crypto"),
+    },
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    },
+};
