52f593910fb0eda8c55b603d630761a1e94eb05b
Florian Ritterhoff <florian@ritterhoff.org>
2022-06-13 21:37:28 +0200

    feat: get revoke working

--- src/components/smime/list.tsx
+++ src/components/smime/list.tsx
@@ -2,31 +2,42 @@ import { AuthenticationResult } from "@azure/msal-browser";
 import { useAccount, useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { Box, Button } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import { PortalApisListSmimeResponseCertificateDetails, SMIMEApi } from "../../api/pki/api";
 import { Configuration } from "../../api/pki/configuration";
 import { authorize } from "../../auth/api";
 import { Config } from "../../config";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function SmimeCertificates() {
     const isAuthenticated = useIsAuthenticated();
     const { instance, accounts } = useMsal();
     const account = useAccount(accounts[0] || {});
-    
+    const [open, setOpen] = React.useState(false);
+    const reason = useRef<TextFieldProps>(null);
     const [pageSize, setPageSize] = React.useState<number>(15);
     const [loading, setLoading] = React.useState(true);
+    const [selection, setSelection] = React.useState<PortalApisListSmimeResponseCertificateDetails | undefined>(undefined);
     const [certificates, setCertificates] = useState([] as PortalApisListSmimeResponseCertificateDetails[]);
 
-    function revoke(item: PortalApisListSmimeResponseCertificateDetails) {
-        if (isAuthenticated && account) {
+    function revoke() {
+        const item = selection;
+        if (isAuthenticated && account && item) {
             authorize(account, instance, ["api://1d9e1166-1c48-4cb2-a65e-21fa9dd384c7/Certificates", "email"], (response: AuthenticationResult) => {
                 if (response && item.serial) {
                     const cfg = new Configuration({ accessToken: response.accessToken });
                     const api = new SMIMEApi(cfg, `https://${Config.PKI_HOST}`);
-                    api.smimeRevokePost({ serial: item.serial, reason: "" }).then(() => {
-                        return;
+                    api.smimeRevokePost({ serial: item.serial, reason: (reason.current?.value as string) }).then(() => {
+                        load();
+                        setSelection(undefined);
+                        setOpen(false);
                     }).catch(() => {
                         return;
                     });
@@ -69,6 +80,15 @@ export default function SmimeCertificates() {
         load();
     }, [account, instance]);
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setSelection(undefined);
+        setOpen(false);
+    };
+
     if (!isAuthenticated) {
         return <div>Please sign in</div>;
     }
@@ -99,7 +119,10 @@ export default function SmimeCertificates() {
             renderCell: (params) => {
                 const row = (params.row as PortalApisListSmimeResponseCertificateDetails);
                 if (row.status !== "revoked") {
-                    return <Button variant="contained" onClick={() => revoke(row)} sx={{ px: 1, mx: 1 }} color="warning" key="revoke"><DeleteIcon /> Revoke</Button>;
+                    return <Button variant="contained" onClick={() => {
+                        setSelection(row);
+                        handleClickOpen();
+                    }} sx={{ px: 1, mx: 1 }} color="warning" key="revoke"><DeleteIcon /> Widerrufen</Button>;
                 }
                 return <></>;
             },
@@ -123,6 +146,29 @@ export default function SmimeCertificates() {
             onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
             rowsPerPageOptions={[5, 15, 25, 50, 100]}
             pagination rows={certificates}></DataGrid>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>SMIME Zertifikat widerrufen</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Sie möchten das SMIME Zertifikat mit Seriennummer widerrufen.
+
+                    Bitte geben Sie einen Grund ein.
+                </DialogContentText>
+                <TextField
+                    inputRef={reason}
+                    autoFocus
+                    margin="dense"
+                    id="reason"
+                    label="Grund"
+                    fullWidth
+                    variant="standard"
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button variant="contained" onClick={handleClose}>Abbrechen</Button>
+                <Button variant="contained" color="warning" onClick={() => revoke()}>Widerrufen</Button>
+            </DialogActions>
+        </Dialog>
         <Button variant="contained" sx={{ mt: 1 }} href="/smime/new">Neues Zertifikat beziehen</Button>
     </Box>;
 }
