d3a5dbbb9d08477cc0258b75a83f44743c58ec34
Florian Ritterhoff <florian@ritterhoff.org>
2022-07-26 20:23:07 +0200

    fix: theming of checkbox and input form

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { checkboxClasses } from "@mui/material/Checkbox";
 
 // A custom theme for this app
 const theme: Theme = createTheme({
@@ -19,7 +20,36 @@ const theme: Theme = createTheme({
         },
     }, shape: {
         borderRadius: 0,
-    }, 
+    },
+    components: {
+        MuiCheckbox: {
+            styleOverrides: {
+                root: {
+                    [`&.${checkboxClasses.checked}`]: {
+                        color: "#FC5555",
+                    },
+                },
+            },
+        },
+        MuiFormLabel: {
+            styleOverrides: {
+                "root": {
+                    "&.Mui-focused": {
+                        color: "#C6C6C6",
+                    },
+                },
+            },
+        },
+        MuiInput: {
+            styleOverrides: {
+                "root": {
+                    "&&:after": {
+                        borderColor: "#C6C6C6",
+                    },
+                },
+            },
+        },
+    },
 });
 
 export default theme;
