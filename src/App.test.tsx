e95380770f452375f8562fa65bed329020108b1a
Florian Ritterhoff <florian@ritterhoff.org>
2022-08-10 12:58:18 +0200

    fix: Include test in router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ import "@testing-library/jest-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
 import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 afterEach(() => {
     // cleanup on exiting
@@ -16,7 +17,9 @@ test("renders App", async () => {
         <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ThemeProvider>);
 
     expect(screen.getByText("Anmelden")).toBeInTheDocument();
