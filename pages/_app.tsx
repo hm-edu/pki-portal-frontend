72da221bd005f5f27fece9cd899e461d6ebcd05e
Florian Ritterhoff <florian@ritterhoff.org>
2022-09-08 06:20:46 +0200

    Try to simplify process.env usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,19 +14,20 @@ import { Session } from "next-auth";
 import { NextComponentType, NextPageContext } from "next";
 import { Router } from "next/router";
 import Head from "next/head";
-import App, { AppContext } from "next/app";
 
 const Offset = styled("div")(({ theme }) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return theme.mixins.toolbar;
 });
-function MyApp({ Component, pageProps: { ...pageProps } }: { pageProps: { session: Session; idp: string } } & {
+function MyApp({ Component, pageProps: { ...pageProps } }: { pageProps: { session: Session } } & {
     Component: NextComponentType<NextPageContext, unknown, Record<string, unknown>>;
     router: Router;
     __N_SSG?: boolean;
     __N_SSP?: boolean;
     __N_RSC?: boolean;
 }) {
+    const idp = process.env["AUTH_IDP"] ?? "https://idp.hmtest.de";
+    
     return <SessionProvider session={pageProps.session} >
         <Head>
             <title>HM Portal</title>
@@ -34,7 +35,7 @@ function MyApp({ Component, pageProps: { ...pageProps } }: { pageProps: { sessio
         <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
-            <ButtonAppBar idp={pageProps.idp} />
+            <ButtonAppBar idp={idp} />
 
             <Offset />
             <Container sx={{ paddingTop: "10px", paddingBottom: "10px" }} maxWidth="xl" >
@@ -43,12 +44,5 @@ function MyApp({ Component, pageProps: { ...pageProps } }: { pageProps: { sessio
         </ThemeProvider>
     </SessionProvider >;
 }
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-    const appProps = await App.getInitialProps(appContext);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    appProps.pageProps.idp = process.env.AUTH_IDP ?? "https://idp.hmtest.de";
-    return { ...appProps };
-};
 
 export default MyApp;
