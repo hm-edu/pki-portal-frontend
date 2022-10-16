9b0a54d360f3732dc6adfc41fa38a42d6a283db6
Florian Ritterhoff <florian@ritterhoff.org>
2022-10-16 15:53:05 +0200

    fix default routes

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export class Config {
     static DOMAIN_HOST: string =
-        process.env.DOMAIN_HOST ?? "https://domain.api.hmtest.de";
+        process.env.DOMAIN_HOST ?? "https://domain.api.hm.edu";
     static PKI_HOST: string =
-        process.env.PKI_HOST ?? "https://pki.api.hmtest.de";
+        process.env.PKI_HOST ?? "https://pki.api.hm.edu";
     static EAB_HOST: string =
-        process.env.EAB_HOST ?? "https://eab.api.hmtest.de";
+        process.env.EAB_HOST ?? "https://eab.api.hm.edu";
 }
 
 export interface AuthProps {
