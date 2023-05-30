6308a16bb2cf86257add9cf913981c50e5b76614
Florian Ritterhoff <florian@ritterhoff.org>
2023-05-30 07:40:15 +0200

    feat: add fallback option for middleware spec

--- components/config.ts
+++ components/config.ts
@@ -1,8 +1,8 @@
 export class Config {
-    static DomainHost: string = process.env.DOMAIN_HOST ?? "https://domain.api.example.edu";
-    static PkiHost: string = process.env.PKI_HOST ?? "https://pki.api.example.edu";
-    static EabHost: string = process.env.EAB_HOST ?? "https://eab.api.example.edu";
-    static AcmeHost: string = process.env.ACME_HOST ?? "https://acme.example.edu";
+    static DomainHost: string = process.env.DOMAIN_HOST ?? process.env.NEXT_PUBLIC_DOMAIN_HOST ?? "https://domain.api.example.edu";
+    static PkiHost: string = process.env.PKI_HOST ?? process.env.NEXT_PUBLIC_PKI_HOST ?? "https://pki.api.example.edu";
+    static EabHost: string = process.env.EAB_HOST ?? process.env.NEXT_PUBLIC_EAB_HOST ?? "https://eab.api.example.edu";
+    static AcmeHost: string = process.env.ACME_HOST ?? process.env.NEXT_PUBLIC_ACME_HOST ?? "https://acme.example.edu";
 }
 
 export interface AuthProps {
