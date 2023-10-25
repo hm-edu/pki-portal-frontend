c0562ea9689b5c33c31dd83bc8e8296992a3ec40
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2023-10-25 14:06:33 +0200

    fix: make csp optional

--- middleware.ts
+++ middleware.ts
@@ -4,23 +4,26 @@ import {
     csp,
     strictDynamic,
     strictInlineStyles,
+    ChainableMiddleware,
 } from "@next-safe/middleware";
 
 import { Config } from "@/components/config";
-
-const securityMiddleware = [
-    csp({
-        directives:
-        {
-            "script-src": ["self", "data:"],
-            "style-src": ["self", "data:"],
-            "img-src": ["self", "data:"],
-            "object-src": ["self", "data:"],
-            // @ts-expect-error Hosts will be set using env at build time
-            "connect-src": [Config.PkiHost, Config.EabHost, Config.DomainHost, "self"],
-        },
-    }),
-    strictDynamic(),
-    strictInlineStyles(),
-];
+let securityMiddleware: ChainableMiddleware[] = [];
+if (process.env.DISABLE_CSP == "true") { /* empty */ } else {
+    securityMiddleware = [
+        csp({
+            directives:
+            {
+                "script-src": ["self", "data:"],
+                "style-src": ["self", "data:"],
+                "img-src": ["self", "data:"],
+                "object-src": ["self", "data:"],
+                // @ts-expect-error Hosts will be set using env at build time
+                "connect-src": [Config.PkiHost, Config.EabHost, Config.DomainHost, "self"],
+            },
+        }),
+        strictDynamic(),
+        strictInlineStyles(),
+    ];
+}
 export default chainMatch(isPageRequest)(...securityMiddleware);
