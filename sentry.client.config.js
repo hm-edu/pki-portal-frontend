69109194274d5578d08a1923c0b635abf2059cfc
Florian Ritterhoff <florian@ritterhoff.org>
2024-04-01 19:10:39 +0200

    feat: start playing with distributed tracing

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -11,6 +11,13 @@ Sentry.init({
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
     tunnel: "/api/error",
+    integrations: [Sentry.browserTracingIntegration()],
+    tracePropagationTargets: [
+        process.env.DOMAIN_HOST ?? process.env.NEXT_PUBLIC_DOMAIN_HOST,
+        process.env.PKI_HOST ?? process.env.NEXT_PUBLIC_PKI_HOST,
+        process.env.EAB_HOST ?? process.env.NEXT_PUBLIC_EAB_HOST,
+        process.env.ACME_HOST ?? process.env.NEXT_PUBLIC_ACME_HOST,
+    ],
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
