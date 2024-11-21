48c33697a4acde3e805091564a5b025fafcd6b83
Florian Ritterhoff <florian@ritterhoff.org>
2024-11-21 16:59:54 +0100

    feat: handle missing ua

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -15,7 +15,7 @@ Sentry.init({
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
     beforeSendTransaction(e) {
-        const isKubeProbe = e.request?.headers && e.request.headers['user-agent'].includes('kube-probe');
+        const isKubeProbe = e.request?.headers && e.request?.headers['user-agent'] && e.request.headers['user-agent'].includes('kube-probe');
         if (isKubeProbe) {
             return null;
         }
